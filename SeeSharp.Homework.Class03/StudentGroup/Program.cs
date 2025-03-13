cf963aa9572b154aa2988fa340aca0b174145551
Pane Trajkov <trajkovpane@outlook.com>
2025-03-14 00:21:59 +0100

    SeeSharp.Homework.Class03
    StudentGroup & SumOfEven

---
+++ SeeSharp.Homework.Class03/StudentGroup/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace StudentGroup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C# Homework | Class 03");
+            Console.WriteLine();
+            Console.WriteLine("Task 02: StudentGroup");
+            Console.WriteLine();
+
+            /*
+                Make a new console application called StudentGroup
+
+                Make 2 arrays called studentsG1 and studentsG2 and fill them with 5 student names.
+                Get a number from the console between 1 and 2 and print the students from that group in the console.
+                Example: studentsG1 ["Zdravko", "Petko", "Stanko", "Branko", "Trajko"]
+                Test Data:
+                Enter student group: ( there are 1 and 2 )
+                1
+                Expected Output:
+                The Students in G1 are:
+                Zdravko
+                Petko
+                Stanko
+                Branko
+                Trajko
+            */
+
+            string[] studentArr1 = { "Ace", "Mace", "Dace", "Vace", "Bace" };
+            string[] studentArr2 = { "Stojan", "Bojan", "Vojdan", "Momir", "Vasko" };
+
+            while (true)
+            {            
+                Console.WriteLine("Please select number 1 or number 2 on your keyboard");
+                Console.WriteLine();
+
+                string input = Console.ReadLine();
+                bool success = int.TryParse(input, out int parsedInput);
+
+                if (parsedInput != 1 && parsedInput != 2)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Invalid option. Pick a number again.");
+                    continue;
+                }
+                else if (parsedInput == 1)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("The name of the students are:");
+                    foreach (string student in studentArr1)
+                    {
+                        Console.WriteLine(student);
+                    }
+                }
+                else if (parsedInput == 2)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("The name of the students are:");
+                    foreach (string student in studentArr2)
+                    {
+                        Console.WriteLine(student);
+                    }
+                }
+                break;
+            }
+        }
+    }
+}
---
+++ SeeSharp.Homework.Class03/SumOfEven/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace SeeSharp.Homework.Class02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C# Homework | Class 03");
+            Console.WriteLine();
+            Console.WriteLine("Task 01: SumOfEven");
+            Console.WriteLine();
+
+            /*
+             Make a console application called SumOfEven. Inside it create an array of 6 integers.
+             Get numbers from the input, find and print the sum of the even numbers from the array:
+            
+            USE FOR LOOP
+            * Test Data:
+              * Enter integer no.1:
+                * 4
+              * Enter integer no.2:
+                * 3
+              * Enter integer no.3:
+                * 7
+              * Enter integer no.4:
+                * 3
+              * Enter integer no.5:
+                * 2
+              * Enter integer no.6:
+                * 8
+            * Expected Output:
+              * The result is: 14
+            */
+
+            int[] intArray = new int[6];
+            int i = 0;
+
+            while (true)
+            {
+                while (i < 6)
+                {
+                    Console.WriteLine("Enter a number:");
+                    string input = Console.ReadLine();
+
+                    bool success = int.TryParse(input, out int value);
+
+                    if (success)
+                    {
+                        intArray[i] = value;
+                        i++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter a valid number.");
+                        continue;
+                    }                                       
+                }
+
+                int result = 0;
+                for (int j = 0; j < intArray.Length; j++)
+                {
+                    if (intArray[j] % 2 == 0)
+                    {
+                        result += intArray[j];
+                    }
+                }
+                Console.WriteLine($"The result is {result}");
+
+
+
+                Console.WriteLine("Do you want to exit the app? (Y/N)");
+                string exitAnswer = Console.ReadLine();
+
+                if (exitAnswer == "Y" || exitAnswer == "y")
+                {
+                    Console.WriteLine("Closing the app...");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Restarting the app...");
+                    i = 0;
+                }
+
+            }
+
+        }
+    }
+}
